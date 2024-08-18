0c9ba7190c5f32de28c140618a39d311d56801d3
msa <mdsohailahmed117@gmail.com>
2024-08-18 05:59:59 +0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ console.log('API Base URL:', import.meta.env.VITE_API_BASE_URL);
  } */
 
  const noonFetch=async()=>{
-  const {data}=await axios.get(`${API_BASE_URL}/api/`)
+  const {data}=await axios.get(`${API_BASE_URL}api/`)
  // const {data}=await axios.get(`/api/`)
   console.log(data)
  }
